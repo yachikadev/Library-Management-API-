b6e88df4b5ddf3042d42a8f3b000e8f236747ba6
Yachika Sharma <shakuntalaramphalsharma@gmail.com>
2026-06-07 23:08:33 +0530

    Register all models in admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Author, Book, Member, IssuedBook
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'genre', 'total_copies', 'available_copies']
+    search_fields = ['title', 'isbn']
+    list_filter = ['genre']
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['user', 'membership_type']
+
+@admin.register(IssuedBook)
+class IssuedBookAdmin(admin.ModelAdmin):
+    list_display = ['book', 'member', 'issued_date', 'due_date', 'return_date', 'fine_amount']
